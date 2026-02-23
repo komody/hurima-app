c3da24e7f5e76df185579a6fbb574deb2f09a4d1
コモディ <nobu54459@gmail.com>
2026-02-23 19:09:58 +0900

    fix/js_file

--- src/resources/views/mypage/profile/edit.blade.php
+++ src/resources/views/mypage/profile/edit.blade.php
@@ -75,24 +75,7 @@
         </div>
     </main>
 
-    <script>
-        document.querySelector('.profile-edit-image-button').addEventListener('click', function(e) {
-            e.preventDefault();
-            document.getElementById('profile_image').click();
-        });
-
-        document.getElementById('profile_image').addEventListener('change', function(e) {
-            const file = e.target.files[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = function(e) {
-                    const imageWrapper = document.querySelector('.profile-edit-image-wrapper');
-                    imageWrapper.innerHTML = '<img src="' + e.target.result + '" alt="プロフィール画像" class="profile-edit-image">';
-                };
-                reader.readAsDataURL(file);
-            }
-        });
-    </script>
+    <script src="{{ asset('js/mypage/profile/edit.js') }}"></script>
 </body>
 
 </html>
--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -12,12 +12,14 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/sell/create.js', 'public/js/sell')
+    .js('resources/js/purchase/show.js', 'public/js/purchase')
+    .js('resources/js/mypage/profile/edit.js', 'public/js/mypage/profile')
     // 共通スタイル
     .sass('resources/css/app.scss', 'public/css')
     // 商品関連
     .sass('resources/css/items/index.scss', 'public/css/items')
     .sass('resources/css/items/show.scss', 'public/css/items')
-    .sass('resources/css/products/index.scss', 'public/css/products')
     // 認証関連
     .sass('resources/css/auth/login.scss', 'public/css/auth')
     .sass('resources/css/auth/register.scss', 'public/css/auth')
